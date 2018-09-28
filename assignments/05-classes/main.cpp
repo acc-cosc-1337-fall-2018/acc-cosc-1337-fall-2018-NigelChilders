78debf731ca9766e8e41eea51c5ddf17ccbb7c18
nigel.childers <nigel.childers@g.austincc.edu>
2018-09-27 19:28:13 -0500

    Updated main to be more than "merely existant", per assignment.

--- assignments/05-classes/main.cpp
+++ assignments/05-classes/main.cpp
@@ -1,5 +1,14 @@
 #include "invoice_detail.h"
+#include <iostream>
 
 int main() {
+	InvoiceDetail ID(10, 10);
+	double value = ID.get_extended_cost();
+
+	std::cout << "10 Units at 10 USD:   $" << value << std::endl;
+
+	std::cout << " \nPress any key to continue\n";
+	std::cin.ignore();
+
 	return 0;
 }
