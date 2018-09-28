bfc94a2ed0370301b5f68959077894ffa57f99d9
nigel.childers <nigel.childers@g.austincc.edu>
2018-09-27 20:15:54 -0500

    travis isn't running tests on vector, hoping this tweak fixes it.

--- homeworks/04-vectors-test/vectors_test.cpp
+++ homeworks/04-vectors-test/vectors_test.cpp
@@ -1,7 +1,6 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "vectors.h"
-#include <iostream>
 
 
 //write test case for get_max_from_vector with a vector of ints 
