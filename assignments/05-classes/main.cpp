bd2a40fa4261835996381c696bf9e698c0048500
nigel.childers <nigel.childers@g.austincc.edu>
2018-10-02 19:13:50 -0500

    Commented out old work for functioning main

--- assignments/05-classes/main.cpp
+++ assignments/05-classes/main.cpp
@@ -2,11 +2,12 @@
 #include <iostream>
 
 int main() {
+/*
 	InvoiceDetail ID(10, 10);
 	double value = ID.get_extended_cost();
 
 	std::cout << "10 Units at 10 USD:   $" << value << std::endl;
-
+*/
 	Invoice Inv;
 
 	Inv.add_invoice_detail(InvoiceDetail(10, 10));
